2d970c55d2d4c086a87142a87dd22a9f5d7bf0ba
zeus_jaydeep <jaydeep.patil@zeuslearning.com>
2026-07-27 14:26:50 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from "express"
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send("<h1>Helooooow World </h1>")
+})
+
+app.listen(4000,()=>{
+    console.log("App started on port 4000")
+})
+
