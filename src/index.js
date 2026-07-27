261a38754f7cc61cf93e47e3deed3d444c5f9ffd
zeus_jaydeep <jaydeep.patil@zeuslearning.com>
2026-07-27 14:38:37 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ app.get('/',(req,res)=>{
     res.send("<h1>Helooooow World </h1>")
 })
 
+app.get('/about',(req,res)=>{
+    res.send("about")
+})
+
 app.listen(4000,()=>{
     console.log("App started on port 4000")
 })
